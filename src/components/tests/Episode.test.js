83b20a0a08e1b17bca3ba646dc516b6d52995ba9
wb1110 <strengthcoachwb@gmail.com>
2021-12-13 14:14:40 -0600

    started hw

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -4,8 +4,15 @@ import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
 
-test("renders without error", () => {});
+test("renders without error", () => {
+    render(<Episode episode={episode}/>)
+});
 
 test("renders the summary test passed as prop", ()=>{});
 
 test("renders default image when image is not defined", ()=>{});
+
+
+// * [ ] Complete a test that shows the Episode component renders. Pass in the provided example episode data as a test prop.
+// * [ ] Modify the test data to display a specific summary statement. Complete a test that shows that the summary value passed in to the Episode component displays as expected. **Use at least then 3 different types of expect statements to test the the existence of the summary value.**
+// * [ ] The episode component displays a default value ('./stranger_things.png') when a image url is not provided. Create a new piece of test data with the image property set to `null`. Test that the alt tag of the image displayed is set to './stranger_things.png'.
